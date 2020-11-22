f6794de30fa8c332631ef69b68c901329eee2f6b
OsmanDeniz <osmandeniz.ceng@gmail.com>
2020-11-22 17:57:49 +0300

    Constructor yazildi.

--- Deniz.StaffTaskManager.WebUI/Controllers/HomeController.cs
+++ Deniz.StaffTaskManager.WebUI/Controllers/HomeController.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Deniz.StaffTaskManager.Businnes.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Deniz.StaffTaskManager.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        ITaskService _taskService;
+        public HomeController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
         public IActionResult Index()
         {
             return View();
