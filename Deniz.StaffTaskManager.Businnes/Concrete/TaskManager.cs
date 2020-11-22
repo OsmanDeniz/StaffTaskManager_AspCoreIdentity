2a44f8317abf5713ddd8e5be379ea1db4d05b4ad
OsmanDeniz <osmandeniz.ceng@gmail.com>
2020-11-22 17:40:10 +0300

    Manager bagimliligi repositoryden, dependecy injection kullanilarak DAL uzerinden dinamik bagimlilik haline getirildi.( Startup dosyasindan dinamik bagimlilik gorulebilir. )

--- Deniz.StaffTaskManager.Businnes/Concrete/TaskManager.cs
+++ Deniz.StaffTaskManager.Businnes/Concrete/TaskManager.cs
@@ -3,40 +3,41 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using Deniz.StaffTaskManager.Businnes.Interfaces;
 using Deniz.StaffTaskManager.DataAccess.Concrete.EntityFramework.Repositories;
+using Deniz.StaffTaskManager.DataAccess.Interfaces;
 using Deniz.StaffTaskManager.Entities.Concrete;
 
 namespace Deniz.StaffTaskManager.Businnes.Concrete
 {
-    class TaskManager : ITaskService
+   public class TaskManager : ITaskService
     {
-        private readonly EfTaskRepository repository;
-        public TaskManager()
+        private readonly ITaskDal _dal;
+        public TaskManager(ITaskDal dal)
         {
-            repository = new EfTaskRepository();
+            _dal = dal;
         }
         public void Add(Task table)
         {
-            repository.Add(table);
+            _dal.Add(table);
         }
 
         public List<Task> GetAll()
         {
-            return repository.GetAll();
+            return _dal.GetAll();
         }
 
         public Task GetById(int id)
         {
-            return repository.GetById(id);
+            return _dal.GetById(id);
         }
 
         public void Remove(Task table)
         {
-            repository.Remove(table);
+            _dal.Remove(table);
         }
 
         public void Update(Task table)
         {
-            repository.Update(table);
+            _dal.Update(table);
         }
     }
 }
