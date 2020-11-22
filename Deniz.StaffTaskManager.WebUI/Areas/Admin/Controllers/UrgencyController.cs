7a89949df011f037b2e20ef35677ab205a501ebc
OsmanDeniz <osmandeniz.ceng@gmail.com>
2020-11-23 00:55:39 +0300

    Aciliyet guncelleme ve Active sinifinin tempdata ile tasinamasi.

--- Deniz.StaffTaskManager.WebUI/Areas/Admin/Controllers/UrgencyController.cs
+++ Deniz.StaffTaskManager.WebUI/Areas/Admin/Controllers/UrgencyController.cs
@@ -20,13 +20,14 @@ namespace Deniz.StaffTaskManager.WebUI.Areas.Admin.Controllers
 
         public IActionResult Index()
         {
+            TempData["active"] = "urgency";
             List<Urgency> urgencies = _urgencyService.GetAll();
             List<UrgencyListViewModel> model = new List<UrgencyListViewModel>();
             foreach (var item in urgencies)
             {
                 UrgencyListViewModel viewModel = new UrgencyListViewModel();
                 viewModel.Id = item.Id;
-                viewModel.UrgancyLevel = item.UrgancyLevel;
+                viewModel.UrgencyLevel = item.UrgencyLevel;
 
                 model.Add(viewModel);
             }
@@ -35,16 +36,46 @@ namespace Deniz.StaffTaskManager.WebUI.Areas.Admin.Controllers
 
         public IActionResult AddUrgency()
         {
+            TempData["active"] = "urgency";
             return View(new UrgencyAddViewModel());
-        } 
+        }
         [HttpPost]
         public IActionResult AddUrgency(UrgencyAddViewModel model)
         {
+            TempData["active"] = "urgency";
             if (ModelState.IsValid)
             {
-               _urgencyService.Add(new Urgency()
+                _urgencyService.Add(new Urgency()
                 {
-                    UrgancyLevel = model.UrgencyLevel
+                    UrgencyLevel = model.UrgencyLevel
+                });
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public IActionResult UpdateUrgency(int id)
+        {
+            TempData["active"] = "urgency";
+            var urgency = _urgencyService.GetById(id);
+            UrgencyUpdateViewModel model = new UrgencyUpdateViewModel
+            {
+                Id = urgency.Id,
+                UrgencyLevel = urgency.UrgencyLevel
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateUrgency(UrgencyUpdateViewModel model)
+        {
+            TempData["active"] = "urgency";
+            if (ModelState.IsValid)
+            {
+                _urgencyService.Update(new Urgency
+                {
+                    Id = model.Id,
+                    UrgencyLevel = model.UrgencyLevel
                 });
                 return RedirectToAction("Index");
             }
---
+++ Deniz.StaffTaskManager.WebUI/Areas/Admin/Models/UrgencyUpdateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Deniz.StaffTaskManager.WebUI.Areas.Admin.Models
+{
+    public class UrgencyUpdateViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Bu alan bos gecilemez")]
+        [Display(Name = "Acileyet Durumu ")]
+        public string UrgencyLevel { get; set; }
+    }
+}
