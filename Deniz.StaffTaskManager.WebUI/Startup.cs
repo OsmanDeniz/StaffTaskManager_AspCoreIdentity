0d4396586172689c132313e584554c29709a4b4e
OsmanDeniz <osmandeniz.ceng@gmail.com>
2020-11-22 17:56:56 +0300

    DI (Dependecy Injection) ve diger ayarlari yapildi.

--- Deniz.StaffTaskManager.WebUI/Startup.cs
+++ Deniz.StaffTaskManager.WebUI/Startup.cs
@@ -1,3 +1,9 @@
+using Deniz.StaffTaskManager.Businnes.Concrete;
+using Deniz.StaffTaskManager.Businnes.Interfaces;
+using Deniz.StaffTaskManager.DataAccess.Concrete.EntityFramework.Contexts;
+using Deniz.StaffTaskManager.DataAccess.Concrete.EntityFramework.Repositories;
+using Deniz.StaffTaskManager.DataAccess.Interfaces;
+using Deniz.StaffTaskManager.Entities.Concrete;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,6 +17,24 @@ namespace Deniz.StaffTaskManager.WebUI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            /// Dependecy Injection yapmak icin 
+            ///  ITaskService gordugunde TaskManager i ornekle ve kullan diyorum.
+            services.AddScoped<ITaskService, TaskManager>();
+            services.AddScoped<IUrgencyService, UrgencyManager>();
+            services.AddScoped<IReportService, ReportManager>();
+
+            /// DI icin manager icerisinde DAL'lari kullanabilmek icin onlari da  
+            /// belirtmem gerekiyor.
+            services.AddScoped<ITaskDal, EfTaskRepository>();
+            services.AddScoped<IUrgencyDAL, EfUrgencyRepository>();
+            services.AddScoped<IReportDAL, EfReportRepository>();
+
+            /// Diger ayarlar,
+            /// - DbContext ayari
+            /// - Identity ayari = appuser ve approle'u entityframeworke taskcontexti kullanarak ekle
+            services.AddDbContext<TaskContext>();
+            services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<TaskContext>();
+
             services.AddControllersWithViews();
         }
 
